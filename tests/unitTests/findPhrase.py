5dfa0cefb2fb14dbc08e85a17643798471b6ad23
PangK <pangkunyuan@hotmail.com>
2018-01-05 20:24:51 +0800

    Development

--- tests/unitTests/findPhrase.py
+++ tests/unitTests/findPhrase.py
@@ -3,9 +3,35 @@
 #从voc中间读出来所有的词
 #分成单字词、2-3字词、4字词、5字以上词4个集合
 
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 voc_file = open('../../data/wiki_phrase2/wiki_voc')
+voc_list = list(voc_file)
 
 uniSet = set()
 biTriSet = set()
 quadSet = set()
 longSet = set()
+
+for line in voc_list:
+    word = line.split(' ')[0].decode()
+    l = len(word)
+    if l == 1:
+        uniSet.add(word)
+    elif l == 2 or l == 3:
+        biTriSet.add(word)
+    elif l == 4:
+        quadSet.add(word)
+    else:
+        longSet.add(word)
+
+print "uniSet", len(uniSet)
+print "biTriSet", len(biTriSet)
+print "quadSet", len(quadSet)
+print "longSet", len(longSet)
+
+for word in quadSet:
+    print word
